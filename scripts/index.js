d3f56d08fa98c80dc76c0834f1e055af07e07e6a
PedroDutraVaz <pedro.dutra@vizio.com>
2020-09-19 03:29:39 -0700

    See more projects button anim w style

--- scripts/index.js
+++ scripts/index.js
@@ -4,33 +4,10 @@ $(document).ready(() => {
     const dropperFadeOut = 250
 
     dropperBtn.on('click', () => {
-        // var curHeight = projectsGrid.height()
-        // projectsGrid.css('height', 'auto')
-        // var autoHeight = projectsGrid.height()
-        // console.log(curHeight, autoHeight)
-        // projectsGrid.height(curHeight).animate({
-        //     height: autoHeight
-        // }, 1000, () => {
-        //     projectsGrid.height('auto')
-        // })
-        projectsGrid.addClass('expanded')
-        
-        // dropperBtn.addClass
-        // const x = dropperBtn.x
-        // cont y = dropperBtn.y
         const drp = document.getElementById('projects-dropper')
         const br = drp.getBoundingClientRect()
-        dropperBtn.css({
-            'position': 'fixed',
-            'bottom': 'auto',
-            'top': br.top,// - $(window).scrollTop()
-            'left': br.left
-            // 'left': dropperBtn.globa,
-            // 'top': dropperBtn.y + window.scrollY
-        })
-        // dropperBtn.fadeOut(dropperFadeOut)
-        // window.setTimeout(() => {
-        //     dropperBtn.css('display', 'none')
-        // }, dropperFadeOut)
+        dropperBtn.addClass('removed')
+
+        $('#projects-grid').addClass('expanded')
     })
 })
