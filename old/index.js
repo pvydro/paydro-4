d85183e69661006839d3d00d81aa8a0fa57192d3
PedroDutraVaz <pedro.dutra@vizio.com>
2020-10-11 15:54:43 -0700

    Revamp, intro header w/ animation, subheader

---
+++ old/index.js
@@ -0,0 +1,13 @@
+$(document).ready(() => {
+    const projectsGrid = $('#project-expansion-holder')
+    const dropperBtn = $('#projects-dropper')
+    const dropperFadeOut = 250
+
+    dropperBtn.on('click', () => {
+        const drp = document.getElementById('projects-dropper')
+        const br = drp.getBoundingClientRect()
+        dropperBtn.addClass('removed')
+
+        $('#projects-grid').addClass('expanded')
+    })
+})
--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,2 @@
 $(document).ready(() => {
-    const projectsGrid = $('#project-expansion-holder')
-    const dropperBtn = $('#projects-dropper')
-    const dropperFadeOut = 250
-
-    dropperBtn.on('click', () => {
-        const drp = document.getElementById('projects-dropper')
-        const br = drp.getBoundingClientRect()
-        dropperBtn.addClass('removed')
-
-        $('#projects-grid').addClass('expanded')
-    })
 })
