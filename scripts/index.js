d73a4222817871f61d7c70aaf47f1293ea4982b5
PedroDutraVaz <pedro.dutra@vizio.com>
2020-10-11 23:35:43 -0700

    Added intro center with center separator and anims, social icons above intro sidepiece

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,3 @@
-// const { cascadia } = require("./cascadia/cascadia")
-
 $(document).ready(() => {
     console.log('drdy')
     cascadia({
@@ -7,25 +5,3 @@ $(document).ready(() => {
         showByDefault: true
     })
 })
-requirejs(["helper/util"], function(util) {
-    console.log('rdy')
-    cascadia({
-        delayIncrement: 250,
-        showByDefault: true
-    })
-});
-
-  
-// define(function (require) {
-//     // Load any app-specific modules
-//     // with a relative require call,
-//     // like:
-//     // var messages = require('./messages');
-
-//     // Load library/vendor modules using
-//     // full IDs, like:
-//     // var print = require('print');
-
-//     // print(messages.getHello());
-//     console.log('Rrdt')
-// });
