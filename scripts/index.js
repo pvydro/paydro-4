16c1d57ad56c3b833aa86d266e9adef2f6b86cd1
PedroDutraVaz <pedro.dutra@vizio.com>
2020-10-12 00:48:21 -0700

    popout element for projects page disappearing when scrolled to

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,30 @@
 $(document).ready(() => {
-    console.log('drdy')
+    // Cascadia elements
     cascadia({
         delayIncrement: 250,
         showByDefault: true
     })
+
+    if (isScrolledIntoView('#projects-page-header', true)) {
+        $('#projects-page-header .popout').css('display', 'none')
+    }
+
+    // Hide popout on scrollY
+    $(document).on('scroll', () => {
+        // console.log('scroll')
+        if (isScrolledIntoView('#projects-page-header', true)) {
+            console.log('here')
+            $('#projects-page-header .popout').addClass('hidden')
+        }
+    })
 })
+
+function isScrolledIntoView(elem, minusHeight) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + (minusHeight ? 0 : $(elem).height())// + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
