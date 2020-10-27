c40f9744f640c87256687ea446d1a4b121cae97e
PedroDutraVaz <pedro.dutra@vizio.com>
2020-10-26 18:37:01 -0700

    Toolboccs, initial implementation, whole lotta project page styling

--- scripts/toolbox.js
+++ scripts/toolbox.js
@@ -1,16 +1,39 @@
 var Toolboccs = (() => {
-    // var navList = $('.toolbox-nav-list')
-    var navs = $('.toolbox-nav-item')
-
-    var indiHeight = 100 / navs.length
+    var indiHeight = 100 / $('.toolbox-nav-item').length
+    var internals = $('.toolbox-internal')
+    var currentlySelected = 0
 
     document.documentElement.style.setProperty('--toolbox-height', indiHeight + "%")
 
+    // mouse enter = go to the thing
     $('.toolbox-nav-item').each((index, ele) => {
         $(ele).hover(() => {
-            console.log('hover', index)
             document.documentElement.style.setProperty('--toolbox-selected', index)
+        })
+        $(ele).on('click', (e) => {
+            // console.log('cicik', index)
+            clearActiveBoxes()
+            currentlySelected = index
+            $(ele).addClass('active')
+            $($('.toolbox-internal')[index]).addClass('active')
         })
     })
 
+    // mouseleave = reset to active
+    $('.toolbox').on('mouseleave', () => {
+        document.documentElement.style.setProperty('--toolbox-selected', currentlySelected)
+    })
+
+    // select first toolbox by default
+    // $('.toolbox-internal').first().addClass('active')
+
+    // $('.toolbox-internal').each((index, ele) => {
+
+    // })
+
+    function clearActiveBoxes() {
+        console.log('clear!')
+        $('.toolbox-nav-item').removeClass('active')
+        $('.toolbox-internal').removeClass('active')
+    }
 })
