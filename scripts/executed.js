dad4a8a66e83c4bbbe3c4c543c8d98b7cf0487ed
PedroDutraVaz <pedro.dutra@vizio.com>
2020-11-05 19:02:05 -0800

    Executed section & cells w/ more info section

---
+++ scripts/executed.js
@@ -0,0 +1,30 @@
+$(document).ready(() => {
+
+    function initExecuted() {
+        addCellListeners()
+    }
+
+    function addCellListeners() {
+        $('.executed-cell-list-item').each((index, ele) => {
+            const el = $(ele)
+            el.on('click', () => {
+                closeAllCells()
+                const showID = el.data('executed-trigger')
+
+                el.parent().addClass('hidden')
+                $('#' + showID).addClass('active')
+            })
+        })
+        $('.executed-cell-more-info-paragraph').on('click', () => {
+            console.log('cliiiii')
+            closeAllCells()
+        })
+    }
+
+    function closeAllCells() {
+        $('.executed-cell-list').removeClass('hidden')
+        $('.executed-cell-more-info-paragraph').removeClass('active')
+    }
+
+    initExecuted()
+})
