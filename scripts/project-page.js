359c22076673c675d448e58fe3be420c3c44197f
PedroDutraVaz <pedro.dutra@vizio.com>
2020-11-07 23:56:16 -0800

    Project showcaser in v pjp

--- scripts/project-page.js
+++ scripts/project-page.js
@@ -1,3 +1,19 @@
 $(document).ready(() => {
     Toolboccs()
+
+    // showcaser content expansion
+    $('.showcaser-bg-clicker-cell').each((index, ele) => {
+        const el = $(ele)
+        el.on('click', () => {
+            console.log('clicked ' + index)
+            $('#project-showcase-a').addClass('video-shown')
+            $('.showcaser-bg-clicker-cell').removeClass('active')
+            el.addClass('active')
+            el.parent().addClass('active')
+        })
+    })
 })
+
+/**
+ * 
+ */
