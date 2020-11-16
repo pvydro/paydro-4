e89838a3737a80361f136e20fd10938f7182c2ac
PedroDutraVaz <pedro.dutra@vizio.com>
2020-11-15 18:28:06 -0800

    All other projects

--- projects/tilting/tilting.js
+++ projects/tilting/tilting.js
@@ -1,6 +1,23 @@
-$(document).ready(() => {
-    console.log('g2go')
+const ids = {
+    ftc: 'ftc',
+    terra: 'terra',
+    lang: 'lang',
+    siege: 'siege'
+}
 
+currentGame = ids.lang
+
+$(document).ready(() => {
+    
+    // Switching sliders
+    $('.other-row .other-cell').on('click', (ev) => {
+        const ele = $(ev.target)
+        const otherId = ele.attr('data-other')
+        currentGame = otherId
+        refreshCurrentSlider()
+    })
+
+    // Screenshot slider
     $('.other-showcase-cell').on('click', (ev) => {
         const cell = $(ev.target)
         console.log('outer clic')
@@ -15,7 +32,10 @@ $(document).ready(() => {
     })
 
     function assignLeftAndRight() {
-        $('.other-showcase-cell:not(.active)').each((i, e) => {
+        const currentid = '#other-showcase-cont-' + currentGame
+        const selector = currentid + ' .other-showcase-cell:not(.active)'
+
+        $(selector).each((i, e) => {
             if (i == 0) {
                 $(e).addClass('left')
             } else if (i == 1) {
@@ -28,6 +48,16 @@ $(document).ready(() => {
         $('.other-showcase-cell').removeClass('active')
         $('.other-showcase-cell').removeClass('right')
         $('.other-showcase-cell').removeClass('left')
+    }
+
+    function refreshCurrentSlider() {
+        clearAll()
+        const currentid = '#other-showcase-' + currentGame
+        $('.other-showcase').removeClass('active')
+        $(currentid).addClass('active')
+        // $(currentid).first('.other-showcase-cell').addClass('active')
+        $(currentid + ' .other-showcase-cell').first().addClass('active')
+        assignLeftAndRight()
     }
 
     assignLeftAndRight()
