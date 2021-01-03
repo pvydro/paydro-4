35844d424d7c07c680e848c209266a33d62f1a38
PedroDutraVaz <pedro.dutra@vizio.com>
2021-01-03 00:23:43 -0800

    Showcaser clicks & fx

--- scripts/project-page.js
+++ scripts/project-page.js
@@ -1,3 +1,7 @@
+let vidPlaying = false
+let vidJustShown = false
+let vidShown = false
+
 $(document).ready(() => {
     Toolboccs()
 
@@ -13,15 +17,55 @@ $(document).ready(() => {
 
         })
         el.on('click', () => {
-            console.log('clicked ' + index)
             $('#project-showcase-a').addClass('video-shown')
             $('.showcaser-bg-clicker-cell').removeClass('active')
             el.addClass('active')
             el.parent().addClass('active')
+
+            if (index !== 1 && vidPlaying) {
+                console.log('Force pause showcase video')
+                pauseShowcaseVideo()
+            } else if (index === 1) {
+                vidShown = true
+                vidJustShown = true
+                setTimeout(() => { vidJustShown = false }, 10)
+            }
         })
+    })
+
+    $('.video-controls .central-button').on('click', () => {
+        if (vidShown && !vidJustShown) {
+            if (vidPlaying) {
+                pauseShowcaseVideo()
+            } else {
+                playShowcaseVideo()
+            }
+        }
     })
 })
 
-/**
- * 
- */
+function pauseShowcaseVideo() {
+    const vidParent = $('#showcaser-vid')
+    const vid = vidParent.get(0)
+    const playBtn = $('.video-controls .central-button .play-button')
+    const pauseBtn = $('.video-controls .central-button .pause-button')
+
+    vidParent.removeClass('video-playing')
+    pauseBtn.removeClass('active')
+    playBtn.addClass('active')
+    vid.pause()
+    vidPlaying = false
+}
+
+function playShowcaseVideo() {
+    const vidParent = $('#showcaser-vid')
+    const vid = vidParent.get(0)
+    const playBtn = $('.video-controls .central-button .play-button')
+    const pauseBtn = $('.video-controls .central-button .pause-button')
+
+    vidParent.addClass('video-playing')
+    pauseBtn.addClass('active')
+    playBtn.removeClass('active')
+    vid.play()
+    vidPlaying = true
+}
